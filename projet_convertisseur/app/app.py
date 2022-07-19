555edc318108007ce7349cd399cc100ea393780d
Senjosakki <ekisols@hotmail.fr>
2022-07-20 01:16:42 +0200

    projet_convertisseur

---
+++ projet_convertisseur/app/app.py
@@ -0,0 +1,100 @@
+from PySide2 import QtWidgets
+import currency_converter
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.c = currency_converter.CurrencyConverter()
+        self.setWindowTitle("Convertisseur de devises")
+        self.setup_ui()  
+        self.set_default_values()
+        self.setup_connections()
+        self.setup_css()
+        self.resize(500, 50)
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.cbb_devisesFrom = QtWidgets.QComboBox()
+        self.spn_montant = QtWidgets.QSpinBox()
+        self.cbb_devisesTo = QtWidgets.QComboBox()
+        self.spn_montantConverti = QtWidgets.QSpinBox()
+        self.btn_inverser =QtWidgets.QPushButton('Inverser devises')
+
+        self.layout.addWidget(self.cbb_devisesFrom)
+        self.layout.addWidget(self.spn_montant)
+        self.layout.addWidget(self.cbb_devisesTo)
+        self.layout.addWidget(self.spn_montantConverti)
+        self.layout.addWidget(self.btn_inverser)
+
+    def set_default_values(self):
+        self.cbb_devisesFrom.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesFrom.setCurrentText('EUR')
+        self.cbb_devisesTo.setCurrentText('EUR')
+
+
+        self.spn_montant.setRange(1, 1000000)
+        self.spn_montantConverti.setRange(1, 1000000)
+        self.spn_montant.setValue(100)
+        self.spn_montantConverti.setValue(100)
+
+    def setup_connections(self):
+        self.cbb_devisesFrom.activated.connect(self.compute)
+        self.cbb_devisesTo.activated.connect(self.compute)
+        self.spn_montant.valueChanged.connect(self.compute)
+        self.btn_inverser.clicked.connect(self.inverser_devise)
+
+    def setup_css(self):
+        self.setStyleSheet("""
+    background-color: rgd(30, 30, 30);
+
+        """)
+
+        self.btn_inverser.setStyleSheet("""
+background-color: rgba(51, 51, 51, 0.05);
+  border-radius: 8px;
+  border-width: 0;
+  color: #333333;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  list-style: none;
+  margin: 0;
+  padding: 10px 12px;
+  text-align: center;
+  transition: all 200ms;
+  vertical-align: baseline;
+  white-space: nowrap;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+
+        """)
+
+    def compute(self):
+        montant = self.spn_montant.value()
+        devise_from = self.cbb_devisesFrom.currentText()
+        devise_to = self.cbb_devisesTo.currentText()
+
+        try:
+            resultat = self.c.convert(montant, devise_from, devise_to)
+        except:
+            print("la conversion n'a pas fonctionnée")
+        else:
+            self.spn_montantConverti.setValue(resultat)
+
+    def inverser_devise(self):
+        devise_from = self.cbb_devisesFrom.currentText()
+        devise_to = self.cbb_devisesTo.currentText()
+
+        self.cbb_devisesFrom.setCurrentText(devise_to)
+        self.cbb_devisesTo.setCurrentText(devise_from)
+
+        self.compute()
+
+app = QtWidgets.QApplication([])
+win = App()
+win.show()
+app.exec_()
