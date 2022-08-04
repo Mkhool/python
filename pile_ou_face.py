b012e59b4b287d527c967f23af4629b2f96f760e
senjosakki <64525410+ekisols@users.noreply.github.com>
2022-08-04 11:53:38 +0200

    maj

--- pile_ou_face.py
+++ pile_ou_face.py
@@ -1,13 +1,20 @@
+from collections import Counter
 from random import random
 mylist = []
-listepile = []
-listeface = []
+
 for i in range(100):
     if random() < 0.50:
         mylist.append("cote_face")
     else:
         mylist.append("cote_pile")
 
+tirage = Counter(mylist)
+
+listepile = print(f'Le coté PILE est tombé {tirage.get("cote_pile")} fois')
+listeface = print(f'Le coté FACE est tombé {tirage.get("cote_face")} fois')
+
+
+"""
 for x in mylist:
     if "cote_pile" in x:
         listepile.append(x)
@@ -16,3 +23,5 @@ for x in mylist:
 
 print(f'le coté pile est tombé {(len(listepile))} fois')
 print(f'le coté face est tombé {(len(listeface))} fois')
+"""
+
