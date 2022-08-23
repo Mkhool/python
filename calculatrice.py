52ae8b7af1b2f18a67858752ae404cdae1824a4c
Senjosakki <ekisols@hotmail.fr>
2022-08-23 22:34:12 +0200

    add try except

--- calculatrice.py
+++ calculatrice.py
@@ -1,4 +1,16 @@
-nombre1 = input("Veuillez entrer un premier nombre : ")
-nombre2 = input("Veuillez entrer un deuxieme nombre : ")
+nombre1 = 0
+nombre2 = 0
 
-print(f"l'addition de {nombre1} et {nombre2} est égal à {int(nombre1) + int(nombre2)}")
+while nombre1 == 0 and nombre2 == 0:
+    nombre1_str = input("Veuillez entrer un premier nombre : ")
+    nombre2_str = input("Veuillez entrer un deuxieme nombre : ")
+
+    try:
+        nombre1 = int(nombre1_str)
+        nombre2 = int(nombre2_str)
+
+    except ValueError:
+        print("ERREUR: Vous devez rentrez un nombre pour l'age.")
+
+nombre3 = int(nombre1_str) + int(nombre2_str)
+print("l'addition de " + str(nombre1_str) + " et " + str(nombre2_str) + " est égal à " + str(nombre3))
